f6e50a673d50e35d97c9849a521ef529174b4b01
Jonathan Tang <jtang002@gold.ac.uk>
2018-11-06 23:51:46 +0000

    refreshing page

--- players.php
+++ players.php
@@ -9,7 +9,8 @@
         } else {
             $message = "Incorrect password \\n Try again.";
             echo "<script type='text/javascript'>alert('$message');</script>";
-            header("Refresh:0");
+            #header("Refresh:0");
+            echo "<meta http-equiv='refresh' content='0'>";
         }
     }
 ?>
