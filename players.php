08888d95863a5daa39609d1bd5f53b4c07299f24
Jonathan Tang <jtang002@gold.ac.uk>
2018-11-06 23:32:22 +0000

    fixing error message on wrong pw

--- players.php
+++ players.php
@@ -9,6 +9,7 @@
         } else {
             $message = "Incorrect password \\n Try again.";
             echo "<script type='text/javascript'>alert('$message');</script>";
+            header("Refresh:0");
         }
     }
 ?>
