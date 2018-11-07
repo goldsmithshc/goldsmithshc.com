af9bfef979bf96673b136168a18c7957f0dedaf2
Jonathan Tang <jtang002@gold.ac.uk>
2018-11-07 00:15:42 +0000

    updating error checking

--- players.php
+++ players.php
@@ -9,7 +9,7 @@
         } else {
             $message = "Incorrect password \\n Try again.";
             echo "<script type='text/javascript'>alert('$message');</script>";
-            #header("Refresh:0");
+            header("Refresh:0");
             #header('Location: players.php');
         }
     }
